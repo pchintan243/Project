354af436cfcb221b338eabe3266416e12a087470
Chintan Patel <patelchintan843@gmail.com>
2023-01-08 14:06:07 +0530

    work properly batter to past commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ async function main() {
     console.log("We are connecting bro..");
 }
 
-// Define mongoose schema
+// Define schema for QueryFrom file --> QueryFrom.html
 const helloSchema = new mongoose.Schema({
     Firstname: String,
     Lastname: String,
@@ -24,11 +24,14 @@ const helloSchema = new mongoose.Schema({
     OtherQuery: String,
     Computer: String,
     Phone: String,
-    Note: String
+    Note: String,
+    QueryDate: Date
 });
 
 const Hello = mongoose.model('Hello', helloSchema);
 
+
+// Define schema for main file --> index.html
 const mainSchema = new mongoose.Schema({
     Email: String,
     Password: String
@@ -36,36 +39,40 @@ const mainSchema = new mongoose.Schema({
 
 const Main = mongoose.model('Main', mainSchema);
 
-// Express specific stuff
-const staticPath = path.join(__dirname)
+
+// Set the path for index file or QueryFrom file
+const staticPath = path.join(__dirname);
 app.use(express.static(staticPath));
+
+// For save the data in to the database
 app.use(express.urlencoded());
 
 
-//Endpoints
+// For main file: index.html
 app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-// app.get('/QueryForm', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/QueryForm.html'));
-// });
+// For QueryForm file: QueryForm.html
+app.get('/QueryForm', (req, res) => {
+    res.sendFile(path.join(__dirname + '/QueryForm.html'));
+});
 
+// For main file: index.html --> It opens the QueryForm.html File
 app.post('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/QueryForm.html')); 
+    res.sendFile(path.join(__dirname + '/QueryForm.html'));
     var myData = new Main(req.body);
     myData.save();
 });
 
-
-
+// For QueryForm file: QueryForm.html --> It sends the conformations your complaints was taken or not
 app.post('/QueryForm.html', (req, res) => {
     res.sendFile(path.join(__dirname + '/QueryForm.html'));
     var myData = new Hello(req.body);
     myData.save().then(() => {
-        res.send("This item has been saved in the database")
+        res.send("Your complaint registered succesfully..!!")
     }).catch(() => {
-        res.status(400).send("Item was not saved to the database")
+        res.status(400).send("Please fill all the detail correctly..!!")
     });
 });
 
