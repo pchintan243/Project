a8ccae098c7e28365f72b978c3ad537c77dfdfe6
Chintan Patel <patelchintan843@gmail.com>
2023-03-18 16:48:18 +0530

    schema modify email

--- src/models/loginSchema.js
+++ src/models/loginSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const loginSchema = new mongoose.Schema({
     Email: {
         type: String,
-        enum: ["kevinpaghadal3@gmail.com", "chintan@gmail.com", "patelchintan843@gmail.com"]
+        // enum: ["kevinpaghadal3@gmail.com", "chintan@gmail.com", "patelchintan843@gmail.com"]
     },
     Password: {
         type: String,
